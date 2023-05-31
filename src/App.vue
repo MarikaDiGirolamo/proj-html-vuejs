e5c3975716f0b4173ffe1ac7f42f4353106083ca
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-31 08:32:19 +0200

    plugged in components

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
+
+import {store} from './data/store'
+
+export default{
+  nome: "App.vue",
+  components: {
+    PageHeader,
+    PageMain,
+    PageFooter
+  },
+  data(){
+    return{
+      store
+    }
+
+  },
+  methods: {
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+
+<template>
+  <PageHeader />
+  <PageMain />
+  <PageFooter />
+</template>
+
+<style scoped lang="scss">
+
+</style>
