7b2d9bb5dd6c65dc971ddad6eb841e279123d220
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-06-01 18:02:30 +0200

    Latest additions

---
+++ src/components/BtnFooter.vue
@@ -0,0 +1,93 @@
+<script>
+import LinkFooter from './LinkFooter.vue';
+export default {
+    name: "BtnFooter",
+    components: {
+        LinkFooter
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+
+}
+</script>
+
+
+<template>
+    <footer>
+        <div class="container-fluid">
+            <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+                <div class="container py-5">
+                    <div class="footer-text">
+                        <h2 class="display-5 border-bottom">CHANGE A LIFE TODAY</h2>
+                        <p>As long as poverty, injustice & inequality persist, none of us can truly rest. It doesn't take
+                            much to change a life. Get in touche today anda start making the difference</p>
+                    </div>
+                    <div class="footer-btn">
+                        <button class="btn" type="button">Volunteer</button>
+                        <button class="btn" type="button">Donate Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <LinkFooter />
+    </footer>
+</template>
+
+
+<style scoped lang="scss">
+@use '../styles/partials/_mixins.scss' as *;
+@use '../styles/partials/_variables.scss' as *;
+
+footer {
+    margin-top: 5rem;
+    padding-top: 5rem;
+}
+
+.container-fluid {
+    width: 100%;
+    height: 100%;
+    background: url(../assets/JumboTron.jpeg) no-repeat center top;
+    @include noFlex(center, center, center)
+}
+
+.container {
+    max-width: 800px;
+
+    h2,
+    p {
+        color: $supportColor;
+        padding: 1rem;
+    }
+
+    p {
+        font-size: 1.3rem;
+        font-weight: lighter;
+    }
+
+
+    .btn {
+        color: $newThemeColor;
+        border: 1px solid #A7BED3;
+        margin: 3rem;
+        background: transparent;
+
+
+        &:hover {
+            background: transparent;
+            color: $newThemeColor;
+            outline-color: $newThemeColor;
+        }
+    }
+
+}
+</style>
